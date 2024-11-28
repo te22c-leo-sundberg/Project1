2ffc30ad327461abc95a5fe144986b5810dc3100
Leo Sundberg <leo.sundberg@elev.ga.ntig.se>
2024-11-28 16:20:50 +0100

    first commit

---
+++ Project1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+int choice;
+
+Console.WriteLine("Choose a number from 0 to infinite and I'll write it out for you");
+choice = GetNumber();
+Console.WriteLine(choice);
+Console.ReadLine();
+
+static int GetNumber()
+{
+    bool success = false; 
+    string input;
+    int result = 0;
+    while (!success)
+    {
+        input = Console.ReadLine();
+        success = int.TryParse(input, out result);
+        if (result < 0)
+        {
+            success = false;
+        }
+    }
+        return result;
+}
+static bool GetBool()
+{
+    bool success = false;
+    string input = "";
+    while (!success)
+    {
+        Console.WriteLine("[Yes] or [No]?");
+        input = Console.ReadLine().ToUpper();
+        if (input == "YES")
+        {
+            return true;
+        }
+        else if (input == "NO")
+        {
+            return false;
+        }
+    }
+    return false;
+}
